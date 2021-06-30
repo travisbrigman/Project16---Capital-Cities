4bd66d2a94c9816b5fa8e1ecc89e9e7d934506b9
travisbrigman <travis1000@gmail.com>
2021-06-30 11:55:34 -0500

    Initial Commit

---
+++ Project16 - Capital Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16 - Capital Cities
+//
+//  Created by Travis Brigman on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
